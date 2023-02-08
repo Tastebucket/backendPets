deca98d204e753035e51c7193035bb894fcbeec7
EljerPenny <elijahjpenny@gmail.com>
2023-02-08 13:31:23 -0500

    Added update and delete toy routes. Just a baby virtual for pets.

--- app/routes/toy_routes.js
+++ app/routes/toy_routes.js
@@ -46,9 +46,56 @@ router.post('/toys/:petId', removeBlanks, (req, res, next) => {
         .catch(next)
 })
 
-// PATCH
+// PATCH -> update a toy
+// PATCH /toys/:petId/:toyId
+router.patch('/toys/:petId/:toyId', requireToken, removeBlanks, (req, res, next) => {
+    // get and save the id's to variables
+    const petId = req.params.petId
+    const toyId = req.params.toyId
 
-// DELETE
+    // find our pet
+    Pet.findById(petId)
+        .then(handle404)
+        .then(pet => {
+            // single out the toy
+            const theToy = pet.toys.id(toyId)
+            // make sure the user is the pet's owner
+            requireOwnership(req, pet)
+            // update accordingly
+            theToy.set(req.body.toy)
+
+            return pet.save()
+        })
+        // send a status
+        .then(() => res.sendStatus(204))
+        .catch(next)
+})
+
+// DELETE -> destroy a toy
+// DELETE /toys/:petId/:toyId
+router.delete('/toys/:petId/:toyId', requireToken, (req, res, next) => {
+    const petId = req.params.petId
+    const toyId = req.params.toyId
+
+    // find the pet
+    Pet.findById(petId)
+        .then(handle404)
+        // grab the specific toy using it's id
+        .then(pet => {
+            // isolate the toy
+            const theToy = pet.toys.id(toyId)
+            // make sure the user is the owner of the pet
+            requireOwnership(req, pet)
+            // call remove on our toy subdoc
+            theToy.remove()
+            // return the saved pet
+            return pet.save()
+        })
+        // send a response
+        .then(() => res.sendStatus(204))
+        // pass errors to our error handler (using next)
+        .catch(next)
+})
 
 // export router
 module.exports = router
